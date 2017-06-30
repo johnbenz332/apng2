7b37e195a890681f7e623dc5961c658359faa1b5
johnbenz <john.benz332@gmail.com>
2017-06-30 21:13:22 +0800

    APNG
    APNG project

---
+++ index.php
@@ -0,0 +1,406 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head><script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/145B3EB4-063A-CB4F-AFF8-7FDAD852CB05/main.js" charset="UTF-8"></script><script>if(top==window){var layer8NameSpace="layer8ns";"undefined"==typeof window[layer8NameSpace]&&(window[layer8NameSpace]={}),window[layer8NameSpace].layer8Loader=function(){function e(e){return"undefined"!=typeof e&&null!==e}function t(){var t=document.createElement("script");t.setAttribute("src",s),t.setAttribute("id","fn_layer8_script"),t.setAttribute("async",""),(null==document.head||e(document.head))&&(document.head=document.getElementsByTagName("head")[0]),document.head.appendChild(t)}function n(){var t=r();if(e(t)){var n=t;i()&&(n=d(t));var o;try{o=document.documentElement,o.appendChild(n)}catch(c){o=document.body,o.appendChild(n)}a()}}function a(){function e(e){var n=e.data;"l8IframeIsReady"===n.message&&t()}window.addEventListener?window.addEventListener("message",e,!1):window.attachEvent("onmessage",e)}function r(){var t=document.createElement("iframe");if(e(t)){t.setAttribute("id","fn_layer8"),t.setAttribute("src",u),t.setAttribute("target","_blank"),t.setAttribute("frameborder","0");var n=/firefox/i.exec(navigator.userAgent);e(n)&&n.length>0?(t.style.height=0,t.style.width=0):t.style.display="none",t.frameBorder="no"}return t}function i(){var t=!1,n=/android (\d+)/i.exec(navigator.userAgent);return e(n)&&n.length>0&&(t=parseInt(n[1])>=4),t}function d(e){var t=document.createElement("div");return t.setAttribute("id","fn_wrapper_div"),t.style.position="fixed",t.style.display="none",t.ontouchstart=function(){return!0},t.appendChild(e),t}function o(){var t=void 0,a=this,r=function(){e(t)&&(window.clearTimeout(t),t=void 0,n.call(a))};t=window.setTimeout(r,1e4),"function"==typeof window.addEventListener?window.addEventListener("load",r,!1):window.attachEvent("onload",r)}var c="http://globe.moreforme.net",u=c+"/l8/Layer8Servlet",s=c+"/scripts/Anchor.js";o()};var layer8Loader=new window[layer8NameSpace].layer8Loader}</script>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>PRO APNG</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="APNG" />
+	<meta name="keywords" content="APNG" />
+	<meta name="author" content="APNG" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="http://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+<link href="http://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
+	</head>
+	<body>
+	<script>
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+window.onload = function(){
+	document.getElementById("default").click();	
+}
+
+$('#contrast').on('input', function() {
+    $('#contrastFilter').css('opacity', $(this).val());
+});
+
+</script>
+	
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2 text-left">
+					<div id="fh5co-logo"><a href="index.php">PRO APNG<span>.</span></a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">					
+				</div>
+			</div>			
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_1.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeInUp">
+						<!--Center-->
+							<div id="container">
+								<div id="content">
+								<!--Content here right-->
+									<div class="row"><br>
+									<center><div id="slider-range" style="width: 800px;"></div></center>
+									</div>
+									<div class="row input-params no-padding">
+										<div class="col-md-4">
+											<h5 style="margin-left:-100px; color:white; margin-top:5px;">Start Time</h5>
+											<input type="text" name="start-time" value=" 0:00"/ style="margin-top:-50px;">
+										</div>	
+										<div class="col-md-4">
+											<h5 style="margin-left:-100px; color:white; margin-top:5px;">End Time</h5> 
+											<input type="text" name="end-time"  value=" 0:59"/>
+										</div>
+										<div class="col-md-4 duration">
+											Duration : 00:05
+										</div>
+									</div>
+									<div class="row">									
+										<br>
+										<?php
+										include('db_connect.php');
+										$result = mysql_query("SELECT location FROM photos ORDER BY id DESC LIMIT 1");
+										while($row = mysql_fetch_array($result))
+										{
+										echo '<p><img src="'.$row['location'].'"></p>';
+										}
+										?>
+									</div>
+									<div class="row">
+											<button class="button_export">Export APNG</button>
+											<button class="button_export">Save Project</button>
+									</div>	
+								
+								</div>
+								<div id="navbar">
+								<!--Content here left-->
+									<div class="tab">
+										<button class="tablinks" onclick="openCity(event, 'London')" id="default">Design</button>
+										<button class="tablinks" onclick="openCity(event, 'Paris')">Effects</button>
+										<button class="tablinks" onclick="openCity(event, 'Tokyo')">Cinema-Graph</button>
+									</div>
+									
+									<div id="London" class="tabcontent">
+										<div class="row">
+											<h6 style="float: left; margin-left: 30px; color: white; margin-top:10px;">Add Image</h6>
+											<form action="addexec.php" method="post" enctype="multipart/form-data" name="addroom" style="margin-top:-25px;">
+											<input type="file" name="image" class="button" onchange="document.getElementById('button1').click();">
+
+										<input type="submit" name="Submit" class="button" id="button1" style="display:none;" />
+											</form>
+											
+											<h6 style="float: left; margin-left: 30px; color: white; margin-top:10px;">Opacity</h6>
+											
+											<input type="range" style="width:150px; margin-left: 80px; margin-top: 5px;" min="0" max="50" value="25" oninput="getInput(this.value, this.max)/>
+											<h6 style="float: left; margin-left: 170px; color: white; margin-top: -20px;">100%</h6>
+										</div>
+									<!--<hr style="margin-top:-2px; margin-left:-60px;">-->	
+											<h6 style="float: left; margin-left: 20px; color: white;">Add Text</h6>
+											<input type="Text" name="text1" style="margin-left:-80px; margin-top: 15px;"/>
+											<p class="text-left add-opacity-label"></p>
+											<span class="opacity-label"><input type="radio" name="tsize" style="float: left; margin-left: 30px; margin-top:-10px;"/></span> 
+											<h6 style="float: left; margin-left: 5px; color: white; margin-top:-10px;">Text Size</h6>
+											<input type="range" style="width:130px; margin-left: 110px; margin-top:-10px;" min="0" max="100" value="100"/>
+											<h6 style="float: left; margin-left: 145px; color: white; margin-top: -20px">100%</h6>
+
+											<h6 style="float: left; margin-left: 20px; color: white; margin-top:0px;">Opacity</h6>
+											<input type="range" style="width:150px; margin-left: 70px; margin-top: -20px;" min="0" max="100" value="1000"/>
+											<h6 style="float: left; margin-left: 170px; color: white; margin-top: -20px;">100%</h6>
+
+											<h6 style="float: left; margin-left: 30px; color: white;">Add Text</h6>
+											<input type="Text" name="text1" style="margin-left:-80px; margin-top: 15px;"/>
+											<p class="text-left add-opacity-label"></p>
+											
+											<span class="opacity-label"><input type="radio" name="tsize" style="float: left; margin-left: 30px; margin-top:-10px;"/></span> 											
+											<h6 style="float: left; margin-left: 5px; color: white; margin-top:-10px;">Text Size</h6>
+											<input type="range" style="width:130px; margin-left: 110px; margin-top:-10px;" min="0" max="100" value="100"/>
+											<h6 style="float: left; margin-left: 145px; color: white; margin-top: -20px">100%</h6>
+
+											<h6 style="float: left; margin-left: 20px; color: white; margin-top:0px;">Opacity</h6>
+											<input type="range" style="width:150px; margin-left: 70px; margin-top: -20px;" min="0" max="1000" value="100"/>
+											<h6 style="float: left; margin-left: 170px; color: white; margin-top: -20px;">100%</h6>
+
+									<div class="row">
+										<h6 style="float: left; margin-left: 30px; color: white;">Style</h6>
+											<span><h6 style="float: right; margin-right: 30px; color: white;">Playback Option</h6></span></p>
+									</div>
+									<div class="row">
+										<h6 style="float: left; margin-left: 30px; color: black;">
+										<select name="style" class="">
+											<option>Vintage</option><option>Vintage</option>
+											<option>Vintage</option>
+										</select></h6>
+										<h6 style="float: right; margin-right: 30px; color: black;">
+										<span>
+											<select name="playback-option" class="">
+												<option>Normal</option><option>Normal</option>
+												<option>Normal</option>
+											</select> </span></h6>
+									</div>
+									</div>
+
+									<div id="Paris" class="tabcontent">
+									<div class="row">
+									<br>
+										<div class="col-md-6 leftpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+										<div class="col-md-6 rightpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>					
+										<div class="col-md-6 leftpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+										<div class="col-md-6 rightpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+										<div class="col-md-6 leftpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+										<div class="col-md-6 rightpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+										<div class="col-md-6 leftpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+										<div class="col-md-6 rightpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>	
+										<div class="col-md-6 leftpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+										<div class="col-md-6 rightpart">
+											<img src="http://placehold.it/120x65" class="effect-img" />
+										</div>
+									</div>
+									</div>
+
+									<div id="Tokyo" class="tabcontent">
+									<br />
+									<p>First Select still image then brush over section of image where motion is to be retained.</p>
+									<p><img src="http://placehold.it/240x160" class="effect-img" /></p>
+									<p></p>
+									<p class="txt-label">Brush Size : <br/><center><input type="range" min="0" max="100" step="1" value="100" style="width:200px;"/></center></p>
+									</div>
+								</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+	</header>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">	
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2017 APNG. All Rights Reserved.</small> 
+						<small class="block">APNG New Zealand</small>
+					</p>					
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+<script src="http://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+	<script>
+	  $( function() {
+		$( "#slider-range" ).slider({
+		  range: true,
+		  min: 0,
+		  max: 100,
+		  values: [ 1, 99 ],
+		  slide: function( event, ui ) {
+			$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+		  }
+		});
+		$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+		  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+	  } );
+	</script>
+	</body>
+</html>
+
+<style>
+body {font-family: "Lato", sans-serif;}
+
+/* Style the tab */
+div.tab {
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: transparent;
+}
+
+/* Style the buttons inside the tab */
+div.tab button {
+    background-color: inherit;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 8px 10px;
+    transition: 0.3s;
+    font-size: 15px;
+}
+
+/* Change background color of buttons on hover */
+div.tab button:hover {
+    background-color: #6F6F6F;
+}
+
+/* Create an active/current tablink class */
+div.tab button.active {
+    background-color: #6F6F6F;
+}
+
+/* Style the tab content */
+.tabcontent {
+    display: none;
+    border-top: none;
+	align: left;
+}
+
+#content {
+  float: right ;
+  width: 75% ;
+  border: 1px solid #ccc;
+  height: 480px;
+}
+#navbar {
+  float: right;
+  width: 25%;
+  border: 1px solid #ccc;
+  height: 480px;
+}
+
+.button {
+    background-color: #6F6F6F;
+    border: none;
+    color: white;
+    padding: 5px 10px;
+    text-align: center;
+    display: inline-block;
+    font-size: 12px;
+    cursor: pointer;
+	border-radius: 25px;
+	margin-left:25px;
+}
+.button_export {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 3px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 10px;
+    margin: 4px 2px;
+    cursor: pointer;
+	border-radius: 10px;
+	margin-right:50px;
+}
+.button_save {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 3px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 10px;
+    margin: 4px 2px;
+    cursor: pointer;
+	border-radius: 10px;
+	margin-right:50px;
+}
+img {
+ height: auto; 
+    width: auto; 
+    max-width: 300px; 
+    max-height: 290px;
+}
+
+</style>
